d5abb81a15a77f87afb6295530a9775687284d3a
Chaitra Dangat <33135541+chaitradangat@users.noreply.github.com>
2017-12-25 01:56:22 +0530

    Refactored Code , File encoding Changes
    Code has been refactored into Python methods or defs

--- SentenceBreaker.py
+++ SentenceBreaker.py
@@ -1,128 +1,137 @@
 #python sentence tokenizer - by Chaitra
-print('')
 
-print('loading dependencies...')
-print('-----------------------------------------------------------------------')
+tokenizer = ''
+selected_language = ''
+
+def loadNltkdependdencies():
+ print('')
+ print('loading dependencies...') 
+ print('-----------------------------------------------------------------------')
+ nltk.download('punkt')
+ print('-----------------------------------------------------------------------')
+ print('loading dependencies completed')
+ print('')
+ print('') 
+
+def loadLanguageList():
+ #variables
+ lang_count = 0 
+ input_language = ''
+ selected_language_ = ''
+ #language list
+ languages_ = ['czech','danish','dutch','english','estonian','finnish','french','german',
+              'greek','italian','norwegian','polish','portuguese','slovene','spanish','swedish','turkish']  
+ print('Language-List')
+ print('-----------------------------------------------------------------------')
+ 
+ for language_ in languages_:
+  lang_count += 1
+  print(str(lang_count) + '.' + language_)
+ print('-----------------------------------------------------------------------')
+ input_language = input('Select A Language:')
+
+ try:
+  if int(input_language) > len(languages_):
+   pass
+  else:
+   selected_language_ = languages_[int(input_language)-1]
+ except ValueError:
+  pass
+ return selected_language_
+ 
+def DisplayProgramHeader():
+ print('Python Sentence Breaker - v1.1 - By Chaitra Dangat')
+ print('')
+
+def LoadSettingFile(selected_language_):
+ tokenizer_ = ''
+ custom_required_ = False
+ print('Setting-File')
+ print('-----------------------------------------------------------------------')
+ try:
+  custom_setting_file = input('Enter custom setting file path(optional):')
+  custom_setting_file = custom_setting_file.replace("\\", "/")
+  tokenizer_ = nltk.data.load('file:' + custom_setting_file)
+  print('custom settings loaded successfully')
+  custom_required_ = True
+ except:
+  print('error loading custom setting file')
+  print('loading default setting file..')
+ if custom_required_ == False:
+  try:
+   #print selected pickle file path 
+   print('')
+   print('settings file path: ' +nltk.data.find('tokenizers/punkt/'+selected_language_+'.pickle'))
+   #load pickle file from nltk data
+   tokenizer_ = nltk.data.load('tokenizers/punkt/'+selected_language_+'.pickle')
+   print('settings loaded successfully...')
+  except:
+   print('***********************************************************************')
+   print('Error loading settings file: \"' + selected_language_+'.pickle' + '\"')
+   print('Please check \"' + selected_language_ +'.pickle' + '\" file exists in:')
+   for path_ in nltk.data.path:
+    print(path_ + '\\tokenizers\\punkt')
+   print('***********************************************************************')
+ else:
+  pass
+ return tokenizer_
+ print('-----------------------------------------------------------------------')
+
+def ProcessFile(input_file_,output_folder_,tokenizer_):
+ output_file = output_folder_ + '\\' + 'output-tokenized-new.txt'
+ output_ = open(output_file, 'w', encoding='iso-8859-1')
+
+ print('Reading Input File...')
+ input_ = codecs.open(input_file_, encoding='iso-8859-1')
+
+ i = 0
+ for line in input_:
+  i += 1
+  if i%100 == 0:
+   print('lines processed-->' + str(i))
+  else:
+   pass
+   pass
+  try:
+   result = tokenizer_.tokenize(line)
+   for op in result:
+    output_.write(op)
+    output_.write('\n')
+  except:
+   pass
+ input_.close()
+ output_.close()
+
 import codecs
 import nltk
-nltk.download('punkt')
+loadNltkdependdencies()
 import nltk.data
-print('-----------------------------------------------------------------------')
-print('loading dependencies completed')
-print('')
-print('')
 
+DisplayProgramHeader()
 
-languages_ = ['czech','danish','dutch','english','estonian','finnish','french','german','greek','italian','norwegian','polish','portuguese','slovene','spanish','swedish','turkish']
-
-lang_count = 0
-selected_language = ''
-input_language = ''
-custom_required = False
-tokenizer = ''
-
-print('Python Sentence Breaker - v1.1 - By Chaitra Dangat')
-print('')
-print('Language-List')
-print('-----------------------------------------------------------------------')
-for language_ in languages_:
-	lang_count += 1
-	print(str(lang_count) + '.' + language_)
-print('-----------------------------------------------------------------------')
-
-input_language = input('Select A Language:')
-
-try: 
-    if int(input_language) > len(languages_):
-     print('invalid selection!')
-     quit()
-    else:
-     selected_language = languages_[int(input_language)-1]
-     print('language selected: '+selected_language)
-     print('')
-except ValueError:
-    print('invalid selection!')
-    quit()
-
-print('Setting-File')
-print('-----------------------------------------------------------------------')
-try:
- custom_setting_file = input('Enter custom setting file path(optional):')
- custom_setting_file = custom_setting_file.replace("\\", "/")
- tokenizer = nltk.data.load('file:' + custom_setting_file)
- print('custom settings loaded successfully')
- custom_required = True
-except:
- print('error loading custom setting file')
- print('loading default setting file..')
-
-if custom_required == False:
- try:
-  #print selected pickle file path 
-  print('')
-  print('settings file path: ' +nltk.data.find('tokenizers/punkt/'+selected_language+'.pickle'))
-  #load pickle file from nltk data
-  tokenizer = nltk.data.load('tokenizers/punkt/'+selected_language+'.pickle')
-  print('settings loaded successfully...')
- except:
-  print('***********************************************************************')
-  print('Error loading settings file: \"' + selected_language+'.pickle' + '\"')
-  print('Please check \"' + selected_language +'.pickle' + '\" file exists in:')
-  for path_ in nltk.data.path:
-   print(path_ + '\\tokenizers\\punkt')
-  print('***********************************************************************')
-  quit() 
+selected_language = loadLanguageList()
+if selected_language != '':
+ print('language selected: '+selected_language)
+ print('')
 else:
+ print('invalid selection!')
+ quit() 
+ 
+tokenizer = LoadSettingFile(selected_language)
+if tokenizer != '':
  pass
-print('-----------------------------------------------------------------------')
+else:
+ print('Unable to load settings!')
+ quit()
  
 print('')
 #input file path
 input_file = input('Enter the text file path :')
 
-#output folder path
+#output file path
 output_folder = input('Enter the output folder path :')
-output_file = output_folder + '\\' + 'output-tokenized.txt'
-
-result_output = []
-output_ = open(output_file, 'w', encoding='utf-8')
-
-print('Reading Input File...')
-
-input_ = codecs.open(input_file, encoding='iso-8859-1')
-
-combined_line = ''
-
-i = 0
-for line in input_:
-    i += 1
-    if i%100 == 0:
-     print('lines processed-->' + str(i))
-    else:
-     pass
-    #if i%1000 == 0:
-    #combined_line += ' ' + line
-    #result = tokenizer.tokenize(combined_line)
-    try:
-     result = tokenizer.tokenize(line)
-     print(result)
-     for op in result:
-      output_.write(op)
-      output_.write('\n')
-    except:
-     pass
-    #combined_line = ''
-    #else:
-    #combined_line += ' ' + line
-
-#if combined_line != '':
-    #result = tokenizer.tokenize(combined_line)
-    #for op in result:
-      #output_.write(op)
-#else:
-  #pass
-
+ 
+ProcessFile(input_file,output_folder,tokenizer)
+ 
 print('Processing Completed')
 input('')
-input_.close()
-output_.close()
